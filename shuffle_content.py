aed7137c91c100d3a2392443a03d4d4785f7537c
Henry Luengas <henry@luengas.dev>
2022-01-06 16:53:47 -0600

    a few quick bug fixes

--- shuffle_content.py
+++ shuffle_content.py
@@ -1,13 +1,16 @@
-import sys, shutil, random, os
-import subprocess
+from sys import exit
+from random import choice
+from os import environ
+from subprocess import Popen, PIPE
+from time import sleep
 
 def main():
-    content_path = "/temp"
+    content_path = "/content"
     tree_command = "tree -vnfio tree.txt " + content_path
     filenames = []
     
     # use the linux tree command to recursively parse the directory structure
-    process = subprocess.Popen(tree_command.split(), stdout=subprocess.PIPE)
+    process = Popen(tree_command.split(), stdout=PIPE)
     _output, _error = process.communicate()
 
     # read the tree output file into a list of filenames
@@ -40,22 +43,23 @@ def main():
             -ac 2
             -f flv
     """
-    ffmpeg_cmd_4 = os.environ.get('STREAM_ADDRESS')
+    ffmpeg_cmd_4 = environ.get('STREAM_ADDRESS')
 
-    ffmpeg_cmd_2 = "-i " + random.choice(filenames)
+    ffmpeg_cmd_2 = "-i " + choice(filenames)
     ffmpeg_cmd = (ffmpeg_cmd_1 + ffmpeg_cmd_2 + ffmpeg_cmd_3 + ffmpeg_cmd_4)
-    process = subprocess.Popen(ffmpeg_cmd.split(), stdout=subprocess.PIPE)
+    process = Popen(ffmpeg_cmd.split(), stdout=PIPE)
     print(filenames)
     print(ffmpeg_cmd.split())
     output, error = process.communicate()
     while (not error):
-        ffmpeg_cmd_2 = "-i " + random.choice(filenames)
+        sleep(10)
+        ffmpeg_cmd_2 = "-i " + choice(filenames)
         ffmpeg_cmd = (ffmpeg_cmd_1 + ffmpeg_cmd_2 + ffmpeg_cmd_3 + ffmpeg_cmd_4)
-        process = subprocess.Popen(ffmpeg_cmd.split(), stdout=subprocess.PIPE)
+        process = Popen(ffmpeg_cmd.split(), stdout=PIPE)
         print(filenames)
         print(ffmpeg_cmd.split())
         output, error = process.communicate()
     
 
 if __name__ == '__main__': 
-    sys.exit(main())
+    exit(main())
