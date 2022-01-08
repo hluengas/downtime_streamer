dda3ce689316ad6311b08f5d0e0a24c63b34911a
Henry Luengas <henry@luengas.dev>
2022-01-07 18:31:59 -0600

    some extras, but still needs work

--- shuffle_content.py
+++ shuffle_content.py
@@ -4,62 +4,132 @@ from os import environ
 from subprocess import Popen, PIPE
 from time import sleep
 
+
 def main():
-    content_path = "/content"
-    tree_command = "tree -vnfio tree.txt " + content_path
-    filenames = []
-    
+    content_path = environ.get("CONTENT_DIR")
+    print(content_path)
+    tree_command = "tree -vnfio /tmp/tree.txt " + content_path
+    video_filenames = []
+
     # use the linux tree command to recursively parse the directory structure
     process = Popen(tree_command.split(), stdout=PIPE)
     _output, _error = process.communicate()
 
-    # read the tree output file into a list of filenames
-    with open('tree.txt') as open_file:
-        files = open_file.readlines()
+    # read the tree output file into a list of video_filenames
+    with open("/tmp/tree.txt") as open_file:
+        all_filenames = open_file.readlines()
 
-    # itterate through and keep only filenames ending in mkv or mp4
-    for file in files:
-        path = file.strip('\n')
+    whitelist = [
+        "archer",
+        "south.park",
+        "rick.and.morty",
+        "futurama",
+        "brickleberry",
+        "gravity.falls",
+        "king.of.the.hill",
+        "samurai.jack",
+        "primal",
+        "cowboy bebop",
+        "justice.league",
+        "one-punch.man",
+        "over.the.garden.wall",
+        "samurai.champloo",
+        "scooby-doo",
+        "solar.opposites"
+    ]
+
+    # itterate through and keep only video_filenames ending in mkv or mp4
+    for file in all_filenames:
+        print(file)
+        path = file.strip("\n")
 
         if path[-4:] in [".mkv", ".mp4"]:
-            filenames.append(path)
+            for series in whitelist:
+                if series in path.lower():
+                    video_filenames.append(path)
 
-    ffmpeg_cmd_1 = """
-        ffmpeg
-        -re
-"""
-    ffmpeg_cmd_3 = """
-            -map 0:v:0
-            -map 0:a:0
-            -map -0:s
-            -framerate 24
-            -c:v libx264
-            -preset medium
-            -crf 16
-            -vf format=yuv420p
-            -g 48
-            -c:a aac
-            -b:a 320k
-            -ac 2
-            -f flv
-    """
-    ffmpeg_cmd_4 = environ.get('STREAM_ADDRESS')
-
-    ffmpeg_cmd_2 = "-i " + choice(filenames)
-    ffmpeg_cmd = (ffmpeg_cmd_1 + ffmpeg_cmd_2 + ffmpeg_cmd_3 + ffmpeg_cmd_4)
-    process = Popen(ffmpeg_cmd.split(), stdout=PIPE)
-    print(filenames)
-    print(ffmpeg_cmd.split())
+    ffmpeg_cmd = [
+        "ffmpeg",
+        "-re",
+        "-i",
+        choice(video_filenames),
+        "-map",
+        "0:v:0",
+        "-map",
+        "0:a:0",
+        "-map",
+        "-0:s",
+        "-map_metadata",
+        "-1",
+        "-framerate",
+        environ.get("VIDEO_FRAMERATE"),
+        "-c:v",
+        "libx264",
+        "-preset",
+        environ.get("VIDEO_ENCODER_PRESET"),
+        "-crf",
+        environ.get("VIDEO_ENCODER_CRF"),
+        "-vf",
+        "format=yuv420p",
+        "-g",
+        environ.get("VIDEO_ENCODER_KEYFRAME_INTERVAL"),
+        "-c:a",
+        "aac",
+        "-b:a",
+        "320k",
+        "-ac",
+        "2",
+        "-f",
+        "flv",
+        environ.get("STREAM_ADDRESS"),
+    ]
+    print(video_filenames)
+    print(ffmpeg_cmd)
+    process = Popen(ffmpeg_cmd, stdout=PIPE)
     output, error = process.communicate()
-    while (not error):
-        sleep(10)
-        ffmpeg_cmd_2 = "-i " + choice(filenames)
-        ffmpeg_cmd = (ffmpeg_cmd_1 + ffmpeg_cmd_2 + ffmpeg_cmd_3 + ffmpeg_cmd_4)
-        process = Popen(ffmpeg_cmd.split(), stdout=PIPE)
-        print(filenames)
-        print(ffmpeg_cmd.split())
-        output, error = process.communicate()
-    
 
-if __name__ == '__main__': 
+    while not error:
+        sleep(10)
+
+        ffmpeg_cmd = [
+            "ffmpeg",
+            "-re",
+            "-i",
+            choice(video_filenames),
+            "-map",
+            "0:v:0",
+            "-map",
+            "0:a:0",
+            "-map",
+            "-0:s",
+            "-map_metadata",
+            "-1",
+            "-framerate",
+            environ.get("VIDEO_FRAMERATE"),
+            "-c:v",
+            "libx264",
+            "-preset",
+            environ.get("VIDEO_AVC_PRESET"),
+            "-crf",
+            environ.get("VIDEO_ENCODER_CRF"),
+            "-vf",
+            "format=yuv420p",
+            "-g",
+            environ.get("VIDEO_ENCODER_KEYFRAME_INTERVAL"),
+            "-c:a",
+            "aac",
+            "-b:a",
+            "320k",
+            "-ac",
+            "2",
+            "-f",
+            "flv",
+            environ.get("STREAM_ADDRESS"),
+        ]
+        print(video_filenames)
+        print(ffmpeg_cmd)
+        process = Popen(ffmpeg_cmd, stdout=PIPE)
+        output, error = process.communicate()
+
+if __name__ == "__main__":
     exit(main())
